55abbe09692488ade11efa0908db10c120effcbc
Sara-Fioretti <fiorettisara22@gmail.com>
2026-09-18 17:17:23 +0200

    Bonus

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import './App.css'
 import products from './assets/products'
 import Form from './assets/components/Form'
 function App() {
- const[productList, setProductList]=useState(products)
-  const addProduct = (newProduct)=>{
+  const [productList, setProductList] = useState(products)
+  const addProduct = (newProduct) => {
     setProductList([...productList, newProduct])
+  }
+  const removeTask = (i) => {
+    const updatedProducts = productList.filter((product, index) => {
+      return index !== i
+    })
+    setProductList(updatedProducts);
   }
   return (
     <>
@@ -15,11 +21,14 @@ function App() {
         <img src="public/img/download.jpg" alt="carrello" />
       </h1>
       <ul>
-        {productList.map((product, i)=>(
-          <li key={i}>{product}</li>
+        {productList.map((product, i) => (
+          <li key={i}>
+            <span >{product}</span>
+            <button onClick={()=>removeTask(i)}>Elimina prodotto</button>
+          </li>
         ))}
       </ul>
-      <Form addProduct={addProduct}></Form>
+      <Form addProduct={addProduct} />
     </>
   )
 }
